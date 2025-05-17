981bd1a365868b3cf15b2aaa2914c9c6aeb08d8f
lealre <renaanvp@gmail.com>
2025-05-17 16:20:10 +0100

    Basic SSE MCP server with one tool

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # filesystem-mcp-go
+
+[mcp-go docs](https://pkg.go.dev/github.com/mark3labs/mcp-go/mcp)
--- main.go
+++ main.go
@@ -1,53 +1,80 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/mark3labs/mcp-go/mcp"
+	"github.com/mark3labs/mcp-go/server"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Print("Usage: go run main.go <path>")
-		os.Exit(1)
-	}
+	mcpServer := server.NewMCPServer(
+		"fs-go-server",
+		"1.0.0",
+		// server.WithResourceCapabilities(true, true),
+		// server.WithPromptCapabilities(true),
+		server.WithToolCapabilities(true),
+		server.WithLogging(),
+	)
 
-	fmt.Println("Printing all args passed")
-	for i, arg := range os.Args {
-		fmt.Printf("Arg %d: %s\n", i, arg)
-	}
-	path := os.Args[1]
+	listEntriesTool := mcp.NewTool("list",
+		mcp.WithDescription("List entries for a given path"),
+		mcp.WithString("path",
+			mcp.Required(),
+			mcp.Description("Path to list all entries"),
+		),
+	)
 
-	listEntries(path)
+	mcpServer.AddTool(listEntriesTool, func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+		path := request.Params.Arguments["operation"].(string)
+
+		entries, err := listEntries(path)
+		if err != nil {
+			return mcp.NewToolResultErrorFromErr("", err), err
+		}
+
+		return mcp.NewToolResultText(entries), nil
+	})
+
+	sseServer := server.NewSSEServer(mcpServer, server.WithBaseURL("http://localhost:8080"))
+	log.Printf("SSE server listening on :8080")
+	if err := sseServer.Start(":8080"); err != nil {
+		log.Fatalf("Server error: %v", err)
+	}
 
 }
 
-func listEntries(path string) error {
+func listEntries(path string) (string, error) {
 	info, err := os.Stat(path)
 	if os.IsNotExist(err) {
-		return fmt.Errorf("path not found")
+		return "", fmt.Errorf("path not found")
 
 	}
 	if err != nil {
-		return fmt.Errorf("error: %s", err)
+		return "", fmt.Errorf("error: %s", err)
 	}
 
 	if !info.IsDir() {
-		return fmt.Errorf("path is not a directory")
+		return "", fmt.Errorf("path is not a directory")
 	}
 
 	entries, err := os.ReadDir(path)
 	if err != nil {
-		return fmt.Errorf("error reading the directory: %s", err)
+		return "", fmt.Errorf("error reading the directory: %s", err)
 	}
 
+	allEntries := ""
 	for _, entry := range entries {
 		pathType := "file"
 		if entry.IsDir() {
 			pathType = "directory"
 		}
-		fmt.Printf("- %s (%s)\n", entry.Name(), pathType)
+		allEntries += fmt.Sprintf("- %s (%s)\n", entry.Name(), pathType)
 	}
 
-	return nil
+	return allEntries, nil
 
 }
